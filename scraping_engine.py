2b8e1ae66a64316cb655d000e75705e4464d215f
jffrr <60816627+jaoferr@users.noreply.github.com>
2021-10-23 12:41:33 -0300

    Added more team stats for extraction

--- scraping_engine.py
+++ scraping_engine.py
@@ -2,7 +2,9 @@ import requests
 import bs4
 import json
 import sys
-
+from collections import defaultdict
+import csv
+import pickle
 
 class NFLSS:
     def __init__(self):
@@ -13,6 +15,7 @@ class NFLSS:
 
     def build_season_url(self, year):
         self.current_url = self.season_url.format(year)
+        self.current_year = year
 
     def build_team_url(self, team_url):
         self.current_team_url = self.base_url + team_url
@@ -33,6 +36,12 @@ class NFLSS:
         ''' Retrieves given table_id's html '''
         current_table = self.soup.find('div', {'class': 'table_wrapper',
                                                'id': table_id})
+
+        # uncomment everything, solves issues with some tables
+        for comment in current_table(text=lambda text: isinstance(text, bs4.Comment)):
+            tag = bs4.BeautifulSoup(comment, 'html.parser')
+            comment.replace_with(tag)
+        
         self.current_table = current_table
         return current_table
 
@@ -59,18 +68,14 @@ class NFLSS:
 
     def extract_season_data(self):
         ''' Extracts data from season page '''
-        data = []
+        data = {}
         tbody = self.current_table.find('tbody')
 
         for row in tbody.find_all('tr', {'class': ''}):
-            team_name = row.find('th').text
-            stat_page_url = row.find('th').find('a')['href']
+            team_name = row.find(attrs={'data-stat': 'team'}).text
+            team_name = team_name.replace('*', '').replace('+', '')
+            current_team = {}
 
-            current_team = {
-                'team_name': team_name,
-                'stat_page_url': stat_page_url
-            }
-            print('\t', team_name)
             # season stats
             for col in row.find_all('td'):  # each column in current row
                 stat = col['data-stat']  # stat name
@@ -78,13 +83,12 @@ class NFLSS:
                 current_team[stat] = stat_value
 
             # team season stats
-            self.build_team_url(stat_page_url)
-            self.make_team_soup()
-            team_stats = self.extract_team_data()
-
-            current_team = {**current_team, **team_stats}
-
-            data.append(current_team)
+            # self.build_team_url(stat_page_url)
+            # self.make_team_soup()
+            # team_stats = self.extract_team_data()
+            # current_team = {**current_team}
+            data[team_name] = current_team
+            
 
         self.current_table_data = data
         return data
@@ -96,6 +100,8 @@ class NFLSS:
 
     def run_multiple_years(self, start_year=2010, end_year=2020):
         ''' Runs from start_year to end_year and outputs to json file'''
+        self.start_year = start_year
+        self.end_year = end_year
         try:  # check args
             start_year = int(start_year)
             end_year = int(end_year)
@@ -107,22 +113,72 @@ class NFLSS:
 
         # if args are correct
         print(f'Running from {start_year} to {end_year}')
-        for year in range(start_year, end_year + 1):
+        all_data = {}
+        for year in range(end_year, start_year - 1, -1):
             print(year)
             self.setup(year)
-            year_data = []
-            for table in ['all_AFC', 'all_NFC']:
-                print('\t', table)
-                self.get_tables(table_id=table)
-                data = self.extract_season_data()
-                year_data += data
+            year_data = defaultdict(dict)
+            tables_to_extract = [
+                'all_AFC', 'all_NFC', 'all_team_stats', 'all_passing',
+                'all_rushing', 'all_returns', 'all_kicking',
+                'all_team_scoring', 'all_team_conversions', 'all_drives'
+            ]
 
-            dump_to_json(year_data, str(year))
+            for table in tables_to_extract:
+                print(f'\t {table:<30}', end='', flush=False)
+                try:
+                    self.get_tables(table_id=table)
+                    data = self.extract_season_data()
+                    print('OK')
+                except TypeError:
+                    print('X')
+                    data = {}
 
+                for team in data.keys():
+                    # print('\t', team)
+                    for stat_name, stat_value in data[team].items():
+                        # print('\t\t', stat_name)
+                        year_data[team][stat_name] = stat_value
 
-def dump_to_json(data, filename='dump'):
-    with open(filename + '.json', 'w') as j:
-        json.dump(data, j)
+            all_data[self.current_year] = year_data
+            # dump_to_json(year_data, str(year))
+            self.data = all_data
+        # dump_to_json(all_data, f'{start_year}-{end_year}')
+        self.dump_to_csv()
+
+    def dump_to_csv(self):
+        exportable_data = []
+        for year in self.data.keys():
+            for team in self.data[year].keys():
+                row = [year, team]
+                for stat in self.data[year][team].values():
+                    row.append(stat)
+                exportable_data.append(row)
+
+        header = ['year', 'team']
+        for year in self.data.keys():
+            for team in self.data[year].keys():
+                for stat in self.data[year][team].keys():
+                    header.append(stat)
+                break
+            break
+
+        exportable_data.insert(0, header)
+
+        filename = f'{self.start_year}-{self.end_year}'
+        with open(filename + '.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(exportable_data)
+
+    def dump_to_pickle(self):
+        filename = f'{self.start_year}-{self.end_year}'
+        with open(filename + '.pickle', "wb") as output_file:
+            pickle.dump(d, output_file)
+
+    def dump_to_json(self):
+        filename = f'{self.start_year}-{self.end_year}'
+        with open(filename + '.json', 'w') as j:
+            json.dump(self.data, j)
 
 
 if __name__ == '__main__':
